08e357a4122b63a3b69eeda2a4a32889ead3f79e
Graham Brooks <graham@grahambrooks.com>
2013-07-29 22:15:14 -0400

    Initial Commit

---
+++ word-finder/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  word-finder
+//
+//  Created by Graham Brooks on 7/29/13.
+//  Copyright (c) 2013 GrahamBrooks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
