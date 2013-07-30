403b950b4601f94cdfac3faa5fa53b143f99e42f
Graham Brooks <graham@grahambrooks.com>
2013-07-30 12:11:30 -0400

    Added some more changes to get a more complete word list

--- word-finder/main.cpp
+++ word-finder/main.cpp
@@ -14,6 +14,17 @@
 
 using namespace std;
 
+void permute(vector<char> chars, std::function<void(const string&)> f) {
+    do {
+        string perm;
+        for (auto x : chars)
+            perm += x;
+        
+        f(perm);
+    } while (next_permutation(chars.begin(), chars.end()));
+}
+
+
 int main(int argc, const char * argv[])
 {
     if (argc < 2) {
@@ -29,28 +40,21 @@ int main(int argc, const char * argv[])
         
         vector<string> real_words;
         
-        do {
-            for (auto i : {3, 4, 5, 6}) {
-                auto j = y.begin();
-                
-                string word;
-                
-                while (i > 0) {
-                    if (*j != ' ') {
-                        word += *j;
-                        i--;
-                    }
-                    j++;
-                }
-                
-                if (found.count(word) == 0) {
-                    if (checker.is_correct(word)) {
-                        real_words.insert(real_words.end(), word);
-                        found[word] = word;
+        permute(y, [&](const string& w) {
+            
+            for (int i = 3; i <= w.size(); i++) {
+                for (int j = 0; j < w.size() - i; j++) {
+                    string to_check(&w.c_str()[j], i);
+                    
+                    if (found.count(to_check) == 0) {
+                        if (checker.is_correct(to_check)) {
+                            real_words.insert(real_words.end(), to_check);
+                            found[to_check] = to_check;
+                        }
                     }
                 }
             }
-        } while (next_permutation(y.begin(), y.end()));
+        });
         
         sort(real_words.begin(), real_words.end());
         
