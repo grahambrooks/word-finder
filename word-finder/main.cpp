030a7b7c2b35c7d6a5ba2194b4dde05d89698cfd
Graham Brooks <graham@grahambrooks.com>
2013-07-30 10:14:35 -0400

    Small updates on creating the vector for permutations

--- word-finder/main.cpp
+++ word-finder/main.cpp
@@ -8,7 +8,8 @@
 
 #include <iostream>
 #include <aspell.h>
-
+#include <vector>
+#include <map>
 using namespace std;
 
 int main(int argc, const char * argv[])
@@ -32,16 +33,43 @@ int main(int argc, const char * argv[])
         } else {
             cout << word << " spelled correctly" << endl;
         }
-
-        word = "kdsjfhsk";
-        correct = aspell_speller_check(spell_checker, word, static_cast<int>(strlen(word)));
-
-        if (correct == 0) {
-            cout << word << " miss-spelled" << endl;
-        } else {
-            cout << word << " spelled correctly" << endl;
-        }
         
+        auto text = " xlirei";
+        
+        vector<char> y(text, text + strlen(text));
+        
+        map<string, string> found;
+        
+        vector<string> real_words;
+        
+        do {
+            for (auto i : {3, 4, 5, 6}) {
+                auto j = y.begin();
+                string word;
+                while (i > 0) {
+                    if (*j != ' ') {
+                        word += *j;
+                        i--;
+                    }
+                    j++;
+                }
+                
+                if (found.count(word) == 0) {
+                    
+                    auto correct = aspell_speller_check(spell_checker, word.c_str(), static_cast<int>(word.length()));
+                    
+                    if (correct) {
+                        real_words.insert(real_words.end(), word);
+                        found[word] = word;
+                     }
+                }
+            }
+        } while (next_permutation(y.begin(), y.end()));
+        
+        sort(real_words.begin(), real_words.end());
+        
+        for(auto g : real_words)
+            cout << g << endl;
         
         delete_aspell_speller(spell_checker);
         delete_aspell_config(spell_config);
