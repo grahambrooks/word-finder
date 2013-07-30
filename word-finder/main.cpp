327a7b18c8834bf7c9795721d7b9a4d5f0c490ce
Graham Brooks <graham@grahambrooks.com>
2013-07-29 22:42:39 -0400

    Small updates to demo spelling

--- word-finder/main.cpp
+++ word-finder/main.cpp
@@ -9,27 +9,43 @@
 #include <iostream>
 #include <aspell.h>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
-
+    
     AspellConfig * spell_config = new_aspell_config();
     
     AspellCanHaveError * possible_err = new_aspell_speller(spell_config);
     
-    AspellSpeller * spell_checker = 0;
+    
     if (aspell_error_number(possible_err) != 0)
         puts(aspell_error_message(possible_err));
-    else
-        spell_checker = to_aspell_speller(possible_err);
-    
-    int correct = aspell_speller_check(spell_checker, "hello", 5);
-    
-    std::cout << "Spell returned " << correct << std::endl;
-    
-    delete_aspell_speller(spell_checker);
-    
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    else {
+        auto spell_checker = to_aspell_speller(possible_err);
+        
+        auto word = "hello";
+        auto correct = aspell_speller_check(spell_checker, word, static_cast<int>(strlen(word)));
+        
+        if (correct == 0) {
+            cout << word << " miss-spelled" << endl;
+        } else {
+            cout << word << " spelled correctly" << endl;
+        }
+
+        word = "kdsjfhsk";
+        correct = aspell_speller_check(spell_checker, word, static_cast<int>(strlen(word)));
+
+        if (correct == 0) {
+            cout << word << " miss-spelled" << endl;
+        } else {
+            cout << word << " spelled correctly" << endl;
+        }
+        
+        
+        delete_aspell_speller(spell_checker);
+        delete_aspell_config(spell_config);
+    }
     return 0;
 }
 
